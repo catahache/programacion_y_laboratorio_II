ea6246eb402f8de61dfced9c33c435b317f3c44a
Catalina <herediacatalina1995@gmail.com>
2021-03-22 15:11:50 -0300

    Ejercicio 15

---
+++ Ejercicio_15/Biblioteca/Calculadora.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        //a. Calcular (público): Recibirá tres parámetros, el primer número, el segundo número y la operación matemática. El método devolverá el resultado de la operación. 
+        public static float Calcular(float num1, float num2, char operacion)
+        {
+            float resultado = 0;
+            switch (operacion)
+            {
+                case '+':
+                    resultado = num1 + num2;
+                    break;
+                case '-':
+                    resultado = num1 - num2;
+                    break;
+                case '*':
+                    resultado = num1 * num2;
+                    break;
+                case '/':
+                    if (Validar(num2))
+                    {
+                        resultado = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error, no se puede dividir por 0");
+                    }
+                   
+                    break;
+            }
+            return resultado;
+        }
+
+        //b. Validar (privado): Recibirá como parámetro el segundo número. Este método se debe utilizar sólo cuando la operación elegida sea la DIVISIÓN. Este método devolverá TRUE si el número es distinto de CERO. 
+        private static bool Validar(float num)
+        {
+            bool valido = false;
+            if (num != 0)
+            {
+                valido = true;
+            }
+            return valido;
+        }
+    }
+}
--- Ejercicio_15/Ejercicio_15/Program.cs
+++ Ejercicio_15/Ejercicio_15/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Biblioteca;
+
 
 // Realizar un programa que permita realizar operaciones matemáticas simples (suma, resta, multiplicación y división). Para ello se le debe pedir al usuario que ingrese dos números y la operación que desea realizar (pulsando el caracter +, -, * ó /). El usuario decidirá cuándo finalizar el programa. Crear una clase llamada Calculadora que posea dos métodos estáticos (de clase): 
 //a. Calcular (público): Recibirá tres parámetros, el primer número, el segundo número y la operación matemática. El método devolverá el resultado de la operación. 
@@ -15,7 +17,40 @@ namespace Ejercicio_15
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio 15";
+            bool respuesta = true;
+            do
+            {
+                bool incorrecto = true;
 
+                Console.Write("Numero 1: ");
+                float.TryParse(Console.ReadLine(), out float num1);
+                Console.Write("Numero 2: ");
+                float.TryParse(Console.ReadLine(), out float num2);
+                Console.Write("Operacion: ");
+                char.TryParse(Console.ReadLine(), out char operacion);
+
+                Console.WriteLine("Resultado: {0}", Biblioteca.Calculadora.Calcular(num1, num2, operacion));
+                
+                do
+                {
+                    Console.WriteLine("Continuar? S/N:");
+                    char.TryParse(Console.ReadLine(), out char letra);
+                    if (char.ToLower(letra) == 'n')
+                    {
+                        respuesta = false;
+                        incorrecto = false;
+                    }
+                    else if (char.ToLower(letra) == 's')
+                    {
+                        incorrecto = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Caracter incorrecto.");
+                    }
+                } while (incorrecto);
+
+            } while (respuesta);
 
 
             Console.ReadKey();
