4268aa49e37b71f1f3544328f3c3a986c0437241
Catalina <herediacatalina1995@gmail.com>
2021-03-23 20:35:50 -0300

    Ejercicio 19

---
+++ Ejercicio_19/Ejercicio_19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 19";
+
+            Sumador sumador = new Sumador();
+            Sumador sumador2 = new Sumador();
+            Console.WriteLine(sumador.Sumar(3, 4));
+            Console.WriteLine(sumador2.Sumar(5, 4));
+            Console.WriteLine(sumador2.Sumar(3, 7));
+            Console.WriteLine(sumador.Sumar("Programacion ", "II"));
+
+            if (sumador | sumador2)
+            {
+                Console.WriteLine("Los sumadores tienen la misma cant de sumas");
+            }
+            else
+            {
+                Console.WriteLine("Los sumadores no tienen la misma cant de sumas");
+
+            }
+
+            Console.WriteLine("La suma de las cantidades de sumas de ambos sumadores es de: {0}", (sumador + sumador2));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_19/Ejercicio_19/Sumador.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador() : this(0) //reutiliza el anterior
+        {
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;//suma una suma
+            return a+b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            return $"{a}{b}";
+        }
+
+        /// <summary>
+        /// Retorna cantidad de sumas de un sumador
+        /// </summary>
+        /// <param name="s"></param>
+        public static explicit operator int(Sumador s)
+        {
+            //recibe un sumador, devuelve un int
+            return s.cantidadSumas;
+        }
+
+        /// <summary>
+        /// Suma dos cantidades de sumas
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static long operator +(Sumador s1, Sumador s2)
+        {
+            return s1.cantidadSumas + s2.cantidadSumas;
+        }
+
+        /// <summary>
+        /// Compara cantidad de sumas de dos sumadores
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static bool operator |(Sumador s1, Sumador s2)
+        {
+            bool mismaCantidad = false;
+            if(s1.cantidadSumas == s2.cantidadSumas)
+            {
+                mismaCantidad = true;
+            }
+            return mismaCantidad;
+        }
+
+    }
+}
