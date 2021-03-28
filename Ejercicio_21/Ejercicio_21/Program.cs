887ae2da9174f3750d0da888dfbee6208c4a0304
Catalina <herediacatalina1995@gmail.com>
2021-03-28 20:32:15 -0300

    Ejercicio 21

---
+++ Ejercicio_21/Ejercicio_21/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnidadesTemperatura;
+
+namespace Ejercicio_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 21";
+
+            Fahrenheit f = new Fahrenheit(93.2);
+            Celsius c = new Celsius(34);
+            Kelvin k = new Kelvin(307.15);
+
+            Console.WriteLine("1 grado Celsius equivale a {0} grados Fahrenheit\n1 grado Kelvin equivale a {1} grados Fahrenheit", Celsius.GetTemperatura(), Kelvin.GetTemperatura());
+
+            Console.WriteLine("Cantidad de grados Fahrenheit: {0}\nCantidad de grados Celsius: {1}\nCantidad de grados Kelvin: {2}", f.GetGrados(), c.GetGrados(), k.GetGrados());
+
+            //Console.WriteLine("Total en Fahrenheit: {0}", ((f+c)+k).GetGrados());
+            //Console.WriteLine("Total Celsius: {0}", ((c+f)+k).GetGrados());
+            //Console.WriteLine("Total en kelvin: {0}",((k+c)+f).GetGrados());
+
+            Console.WriteLine("{0} Fahrenheit son {1} Celsius", f.GetGrados(), ((Celsius)f).GetGrados());
+            Console.WriteLine("{0} Kelvin son {1} Celsius", k.GetGrados(), ((Celsius)k).GetGrados());
+
+            Console.WriteLine("Total Celsius: {0}", ((c + f) + k).GetGrados());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_21/UnidadesTemperatura/Celsius.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnidadesTemperatura
+{
+    public class Celsius
+    {
+        private double grados;
+        private static double tempRespectoFahrenheit;
+
+        static Celsius()
+        {
+            tempRespectoFahrenheit = 33.8;
+        }
+
+        public Celsius(double grados)
+        {
+            this.grados = grados;
+        }
+
+        public Celsius(double grados, double temperatura)
+            : this(grados)
+        {
+            tempRespectoFahrenheit = temperatura;
+        }
+
+        public double GetGrados()
+        {
+            return this.grados;
+        }
+
+        public static double GetTemperatura()
+        {
+            return tempRespectoFahrenheit;
+        }
+
+        //Conversiones:
+        public static implicit operator Celsius(double c)
+        {
+            return new Celsius(c);
+        }
+
+        public static explicit operator Fahrenheit(Celsius c)
+        {
+            return new Fahrenheit(c.grados * 9/5 + 32);
+        }
+
+        public static explicit operator Kelvin(Celsius c)
+        {
+            return new Kelvin((((Fahrenheit)c).GetGrados() + 459.67) * 5/9);
+        }
+
+        //Operaciones
+        public static Celsius operator -(Celsius c, Fahrenheit f)
+        {
+            return new Celsius(c.grados - ((Celsius)f).grados);
+        }
+
+        public static Celsius operator -(Celsius c, Kelvin k)
+        {
+            return new Celsius(c.grados - ((Celsius)k).grados);
+        }
+
+        public static Celsius operator +(Celsius c, Fahrenheit f)
+        {
+            return new Celsius(c.grados + ((Celsius)f).grados);
+        }
+
+        public static Celsius operator +(Celsius c, Kelvin k)
+        {
+            return new Celsius(c.grados + ((Celsius)k).grados);
+        }
+
+
+        //Comparaciones
+        public static bool operator ==(Celsius c, Fahrenheit f)
+        {
+            return c.grados == ((Celsius)f).grados;
+        }
+        public static bool operator !=(Celsius c, Fahrenheit f)
+        {
+            return !(c == f);
+        }
+        public static bool operator ==(Celsius c, Kelvin k)
+        {
+            return c.grados == ((Celsius)k).grados;
+        }
+        public static bool operator !=(Celsius c, Kelvin k)
+        {
+            return !(c == k);
+        }
+        public static bool operator ==(Celsius c1, Celsius c2)
+        {
+            return c1.grados == c2.grados;
+        }
+        public static bool operator !=(Celsius c1, Celsius c2)
+        {
+            return !(c1 == c2);
+        }
+    }
+}
