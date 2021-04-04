6b751b628dbe1cdce041f82bef7ac9a5a0fff8f3
Catalina <herediacatalina1995@gmail.com>
2021-04-04 10:34:28 -0300

    Ejercicio 24

--- Ejercicio_24/FrmTemperatura/Form1.Designer.cs
+++ Ejercicio_24/FrmTemperatura/Form1.Designer.cs
@@ -28,13 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblFahrenheit = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtBCelsius = new System.Windows.Forms.TextBox();
+            this.txtBKelvin = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBFaF = new System.Windows.Forms.TextBox();
+            this.txtBCaF = new System.Windows.Forms.TextBox();
+            this.txtBKaF = new System.Windows.Forms.TextBox();
+            this.txtBFaC = new System.Windows.Forms.TextBox();
+            this.txtBCaC = new System.Windows.Forms.TextBox();
+            this.txtBKaC = new System.Windows.Forms.TextBox();
+            this.txtBFaK = new System.Windows.Forms.TextBox();
+            this.txtBCaK = new System.Windows.Forms.TextBox();
+            this.txtBKaK = new System.Windows.Forms.TextBox();
+            this.btnFahrenheit = new System.Windows.Forms.Button();
+            this.btnCelsius = new System.Windows.Forms.Button();
+            this.btnKelvin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFahrenheit
+            // 
+            this.lblFahrenheit.AutoSize = true;
+            this.lblFahrenheit.Location = new System.Drawing.Point(13, 38);
+            this.lblFahrenheit.Name = "lblFahrenheit";
+            this.lblFahrenheit.Size = new System.Drawing.Size(57, 13);
+            this.lblFahrenheit.TabIndex = 0;
+            this.lblFahrenheit.Text = "Fahrenheit";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Celsius";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kelvin";
+            // 
+            // txtBFahrenheit
+            // 
+            this.txtBFahrenheit.Location = new System.Drawing.Point(91, 38);
+            this.txtBFahrenheit.Name = "txtBFahrenheit";
+            this.txtBFahrenheit.Size = new System.Drawing.Size(70, 20);
+            this.txtBFahrenheit.TabIndex = 3;
+            // 
+            // txtBCelsius
+            // 
+            this.txtBCelsius.Location = new System.Drawing.Point(91, 67);
+            this.txtBCelsius.Name = "txtBCelsius";
+            this.txtBCelsius.Size = new System.Drawing.Size(70, 20);
+            this.txtBCelsius.TabIndex = 4;
+            // 
+            // txtBKelvin
+            // 
+            this.txtBKelvin.Location = new System.Drawing.Point(91, 97);
+            this.txtBKelvin.Name = "txtBKelvin";
+            this.txtBKelvin.Size = new System.Drawing.Size(70, 20);
+            this.txtBKelvin.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Fahrenheit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(317, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Celsius";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(403, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Kelvin";
+            // 
+            // txtBFaF
+            // 
+            this.txtBFaF.Location = new System.Drawing.Point(235, 38);
+            this.txtBFaF.Name = "txtBFaF";
+            this.txtBFaF.Size = new System.Drawing.Size(70, 20);
+            this.txtBFaF.TabIndex = 9;
+            // 
+            // txtBCaF
+            // 
+            this.txtBCaF.Location = new System.Drawing.Point(235, 67);
+            this.txtBCaF.Name = "txtBCaF";
+            this.txtBCaF.Size = new System.Drawing.Size(70, 20);
+            this.txtBCaF.TabIndex = 10;
+            // 
+            // txtBKaF
+            // 
+            this.txtBKaF.Location = new System.Drawing.Point(235, 97);
+            this.txtBKaF.Name = "txtBKaF";
+            this.txtBKaF.Size = new System.Drawing.Size(70, 20);
+            this.txtBKaF.TabIndex = 11;
+            // 
+            // txtBFaC
+            // 
+            this.txtBFaC.Location = new System.Drawing.Point(320, 38);
+            this.txtBFaC.Name = "txtBFaC";
+            this.txtBFaC.Size = new System.Drawing.Size(70, 20);
+            this.txtBFaC.TabIndex = 12;
+            // 
+            // txtBCaC
+            // 
+            this.txtBCaC.Location = new System.Drawing.Point(320, 67);
+            this.txtBCaC.Name = "txtBCaC";
+            this.txtBCaC.Size = new System.Drawing.Size(70, 20);
+            this.txtBCaC.TabIndex = 13;
+            // 
+            // txtBKaC
+            // 
+            this.txtBKaC.Location = new System.Drawing.Point(320, 97);
+            this.txtBKaC.Name = "txtBKaC";
+            this.txtBKaC.Size = new System.Drawing.Size(70, 20);
+            this.txtBKaC.TabIndex = 14;
+            // 
+            // txtBFaK
+            // 
+            this.txtBFaK.Location = new System.Drawing.Point(406, 38);
+            this.txtBFaK.Name = "txtBFaK";
+            this.txtBFaK.Size = new System.Drawing.Size(70, 20);
+            this.txtBFaK.TabIndex = 15;
+            // 
+            // txtBCaK
+            // 
+            this.txtBCaK.Location = new System.Drawing.Point(406, 67);
+            this.txtBCaK.Name = "txtBCaK";
+            this.txtBCaK.Size = new System.Drawing.Size(70, 20);
+            this.txtBCaK.TabIndex = 16;
+            // 
+            // txtBKaK
+            // 
+            this.txtBKaK.Location = new System.Drawing.Point(406, 97);
+            this.txtBKaK.Name = "txtBKaK";
+            this.txtBKaK.Size = new System.Drawing.Size(70, 20);
+            this.txtBKaK.TabIndex = 17;
+            // 
+            // btnFahrenheit
+            // 
+            this.btnFahrenheit.Location = new System.Drawing.Point(167, 35);
+            this.btnFahrenheit.Name = "btnFahrenheit";
+            this.btnFahrenheit.Size = new System.Drawing.Size(62, 23);
+            this.btnFahrenheit.TabIndex = 18;
+            this.btnFahrenheit.Text = "->";
+            this.btnFahrenheit.UseVisualStyleBackColor = true;
+            this.btnFahrenheit.Click += new System.EventHandler(this.btnFahrenheit_Click);
+            // 
+            // btnCelsius
+            // 
+            this.btnCelsius.Location = new System.Drawing.Point(167, 64);
+            this.btnCelsius.Name = "btnCelsius";
+            this.btnCelsius.Size = new System.Drawing.Size(62, 23);
+            this.btnCelsius.TabIndex = 19;
+            this.btnCelsius.Text = "->";
+            this.btnCelsius.UseVisualStyleBackColor = true;
+            this.btnCelsius.Click += new System.EventHandler(this.btnCelsius_Click);
+            // 
+            // btnKelvin
+            // 
+            this.btnKelvin.Location = new System.Drawing.Point(167, 95);
+            this.btnKelvin.Name = "btnKelvin";
+            this.btnKelvin.Size = new System.Drawing.Size(62, 23);
+            this.btnKelvin.TabIndex = 20;
+            this.btnKelvin.Text = "->";
+            this.btnKelvin.UseVisualStyleBackColor = true;
+            this.btnKelvin.Click += new System.EventHandler(this.btnKelvin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(483, 144);
+            this.Controls.Add(this.btnKelvin);
+            this.Controls.Add(this.btnCelsius);
+            this.Controls.Add(this.btnFahrenheit);
+            this.Controls.Add(this.txtBKaK);
+            this.Controls.Add(this.txtBCaK);
+            this.Controls.Add(this.txtBFaK);
+            this.Controls.Add(this.txtBKaC);
+            this.Controls.Add(this.txtBCaC);
+            this.Controls.Add(this.txtBFaC);
+            this.Controls.Add(this.txtBKaF);
+            this.Controls.Add(this.txtBCaF);
+            this.Controls.Add(this.txtBFaF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBKelvin);
+            this.Controls.Add(this.txtBCelsius);
+            this.Controls.Add(this.txtBFahrenheit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFahrenheit);
+            this.Name = "Form1";
+            this.Text = "Temperatura";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFahrenheit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBFahrenheit;
+        private System.Windows.Forms.TextBox txtBCelsius;
+        private System.Windows.Forms.TextBox txtBKelvin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBFaF;
+        private System.Windows.Forms.TextBox txtBCaF;
+        private System.Windows.Forms.TextBox txtBKaF;
+        private System.Windows.Forms.TextBox txtBFaC;
+        private System.Windows.Forms.TextBox txtBCaC;
+        private System.Windows.Forms.TextBox txtBKaC;
+        private System.Windows.Forms.TextBox txtBFaK;
+        private System.Windows.Forms.TextBox txtBCaK;
+        private System.Windows.Forms.TextBox txtBKaK;
+        private System.Windows.Forms.Button btnFahrenheit;
+        private System.Windows.Forms.Button btnCelsius;
+        private System.Windows.Forms.Button btnKelvin;
     }
 }
 
--- Ejercicio_24/FrmTemperatura/Form1.cs
+++ Ejercicio_24/FrmTemperatura/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using UnidadesTemperatura;
+
 namespace FrmTemperatura
 {
     public partial class Form1 : Form
@@ -15,6 +17,49 @@ namespace FrmTemperatura
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnFahrenheit_Click(object sender, EventArgs e)
+        {
+            bool isNumeric = double.TryParse(txtBFahrenheit.Text, out double f);
+            if (isNumeric)
+            {
+                txtBFaF.Text = f.ToString();
+                txtBFaC.Text = (((Celsius)((Fahrenheit)f)).GetGrados()).ToString();
+                txtBFaK.Text = (((Kelvin)((Fahrenheit)f)).GetGrados()).ToString();
+            }
+            else
+            {
+                //txtBFahrenheit.Focus();
+            }
+        }
+
+        private void btnCelsius_Click(object sender, EventArgs e)
+        {
+            bool isNumeric = double.TryParse(txtBCelsius.Text, out double c);
+            if (isNumeric)
+            {
+                txtBCaC.Text = c.ToString();
+                txtBCaF.Text = (((Fahrenheit)((Celsius)c)).GetGrados()).ToString();
+                txtBCaK.Text = (((Kelvin)((Celsius)c)).GetGrados()).ToString();
+            }
+            else
+            {
+            }
+        }
+
+        private void btnKelvin_Click(object sender, EventArgs e)
+        {
+            bool isNumeric = double.TryParse(txtBKelvin.Text, out double k);
+            if (isNumeric)
+            {
+                txtBKaK.Text = k.ToString();
+                txtBKaF.Text = (((Fahrenheit)((Kelvin)k)).GetGrados()).ToString();
+                txtBKaC.Text = (((Celsius)((Kelvin)k)).GetGrados()).ToString();
+            }
+            else
+            {
+            }
         }
     }
 }
